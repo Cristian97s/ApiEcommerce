9826a1f62c36392e185b3c89ab6287f65e7ee332
Cristian97s <cristianaleman1997@gmail.com>
2026-02-05 20:07:17 -0600

    feat: soporte para uso de identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,9 +8,16 @@ public class ApplicationDbContext : DbContext
     {
 
     }
+    // sobre escritura del modelo
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 
     //tabla para la Bb por convencion siempre en plural
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<User> Users { get; set; }
+    // configuracion para trabajar con usarios personalizados utilizando identity
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
