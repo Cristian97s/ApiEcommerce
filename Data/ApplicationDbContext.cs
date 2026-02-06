487c6fb195a39798995f0cecab96348da9c04ba6
Cristian97s <cristianaleman1997@gmail.com>
2026-02-05 20:23:05 -0600

    feat: Migracion para uso de identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,8 @@
 using ApiEcommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     // Constructor
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
